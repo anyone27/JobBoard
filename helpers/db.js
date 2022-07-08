90cd435aa86060888b2bd868a916f8e3c928699f
anyone27 <josh@joshcallarman.com>
2022-07-08 11:48:58 +0100

    db connections

--- helpers/db.js
+++ helpers/db.js
@@ -2,7 +2,7 @@ import mysql from 'serverless-mysql';
 const db = mysql({
 	config: {
 		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
+		// port: process.env.DB_PORT,
 		database: process.env.MYSQL_DATABASE,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASS,
